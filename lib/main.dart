43bf70f17c4d7028521cb767e277e51de4eee99b
JahagirdarPrajwal <jprajwal2004@gmail.com>
2023-12-14 17:07:53 +0530

    asstes

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Trash Hub',
       theme: ThemeData(
         textTheme: const TextTheme(
